236f73c6abfab76972f091cb90728c7612600d11
Junior Silva <hjuniorbsilva@gmail.com>
2025-02-26 16:39:18 -0300

    feat: adicionado rota para retornar todos os usuários

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,6 +12,11 @@ Route::prefix('api')->group(function(){
         // Route::post('/login', [AuthController::class, 'login']);
         Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
+    });
+
+
+    Route::middleware(AuthMiddleware::class)->group(function(){
+        Route::get('users', [UserController::class, 'users']);
     });
 });
 
