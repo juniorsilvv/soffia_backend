c002cb0dba9ce01b6747bd49136610d1e9bce981
Junior Silva <hjuniorbsilva@gmail.com>
2025-02-26 16:48:33 -0300

    feat: adicionado função para cadastrar usuários

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UsersRequest;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\UserCollection;
+use App\ResponseTrait;
 
 class UserController extends Controller
 {
+
+    use ResponseTrait;
     /**
      * Retorna todos os usuários
      * @author Junior <hjuniorbsilva@gmail.com>
@@ -16,5 +21,23 @@ class UserController extends Controller
     {
         $users = User::all();
         return new UserCollection($users);
+    }
+
+    public function create(UsersRequest $request)
+    {
+        try {
+            $user = User::create([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'phone'    => $request->phone,
+                'password' => Hash::make($request->password), 
+            ]);
+        
+            return $this->responseJSON(true, 'Usuário cadastrado com sucesso', 201, [
+                'user'  => $user,
+            ]);            
+        } catch (\Exception $e) {
+            return $this->responseJSON(false, 'Erro ao cadastrar o usuário. Verifique os dados fornecidos.', 500);
+        }
     }
 }
