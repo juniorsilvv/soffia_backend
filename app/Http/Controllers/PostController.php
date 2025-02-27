5baa7eb45f9f39b9755773650a67889a2c2c8aa8
Junior Silva <hjuniorbsilva@gmail.com>
2025-02-27 11:10:30 -0300

    feat: criado rota e função para deletar post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,4 +43,30 @@ class PostController extends Controller
             return $this->responseJSON(false, 'Erro ao cadastrar o Post. Verifique os dados fornecidos.', 500);
         }
     }
+
+
+    /**
+     * Remove o post
+     *
+     * @param [type] $id
+     * @return object
+     * @author Junior <hjuniorbsilva@gmail.com>
+     */
+    public function delete($id) : object
+    {
+        try {
+            $user = $this->postRepository->find($id);
+
+            if (!$user) {
+                return $this->responseJSON(false, 'Post não encontrado', 404);
+            }
+
+            // Deletar o post
+            $this->postRepository->delete($id);
+
+            return $this->responseJSON(true, 'Post deletado com sucesso', 200);
+        } catch (\Exception $e) {
+            return $this->responseJSON(false, 'Erro ao deletar o post.', 500);
+        }
+    }
 }
