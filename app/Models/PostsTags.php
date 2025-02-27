f5914d9d71df30a39e55f092062628c52565aa1d
Junior Silva <hjuniorbsilva@gmail.com>
2025-02-27 15:14:07 -0300

    feat: adicionando hasfactory no poststags

--- app/Models/PostsTags.php
+++ app/Models/PostsTags.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PostsTags extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['post_id', 'tag_name'];
     protected $table = 'posts_tags';
 }
